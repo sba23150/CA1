1e46f45f26ab3d0575481445e8a7de0bd77397f4
sba23150 <sba23150@student.cct.ie>
2024-01-13 19:52:49 +0000

    Menu
    Initialization of Menu

--- src/ca1/CA1.java
+++ src/ca1/CA1.java
@@ -4,17 +4,76 @@
  */
 package ca1;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
- * @author User
+ * @author Tatiana Mota
  */
 public class CA1 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        // The program has a menu that lets the user decide between standard operation or adding (validated) data to the status.txt file via the console. (Invalid data should NOT be saved).
+        //Asking the user to choose 1-Standart Operation or 2-To add data manually
+        System.out.println("Please choose 1 to Standard Operation (use students.text) or 2 to add data to the status.txt file via the console");
+        
+        //Using Scanner to read user choice
+        Scanner myScanner = new Scanner (System.in);
+        int userChoice = 0;
+        try{      
+           userChoice = myScanner.nextInt();
+        }
+        catch(Exception e){
+            System.out.println("Something went wrong.");
+        }
+        
+        //Checking userChoice to initializate program
+        switch (userChoice) {
+            case 1:
+                //call choice1 method
+                choice1();
+                break;
+            case 2:
+                //call choice2 method
+                choice2();
+                break;
+            default:
+                System.out.println("Something went wrong.");
+                break;
+        }
     }
     
+    public static void choice1() {
+
+        BufferedReader myReader = null;
+        try {
+            //Using BufferedReader to read student.txt. 
+            myReader = new BufferedReader (new FileReader("student.txt"));
+            //Read the text and make it into an array
+            String studentsDetails = myReader.readLine();
+            String [] studentsDetailsArray = studentsDetails.split(" ");
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(CA1.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                myReader.close();
+            } catch (IOException ex) {
+                Logger.getLogger(CA1.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
+       
+        
+    }
+    
+    public static void choice2() {
+        
+    }
 }
